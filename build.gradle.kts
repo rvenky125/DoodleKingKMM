dbfa60d7fcb7fc618a345128cfb170cfe19ff67a
rvenky125 <venkypaithireddy@gmail.com>
2023-12-31 23:29:04 +0530

    running configuration in both android and ios

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version("8.0.2").apply(false)
     id("com.android.library").version("8.0.2").apply(false)
-    kotlin("android").version("1.9.21").apply(false)
-    kotlin("multiplatform").version("1.9.21").apply(false)
+    kotlin("android").version("1.9.20").apply(false)
+    kotlin("multiplatform").version("1.9.20").apply(false)
     kotlin("plugin.serialization").version("1.8.0").apply(false)
 
     id("org.jetbrains.compose") apply false
